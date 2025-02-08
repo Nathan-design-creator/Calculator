d36bb11dd608ac2da75a0a73e7801237f7c35645
Nathan-design-creator <anyiamchidumebi@gmail.com>
2025-02-08 19:50:58 +0100

    calculator 2 activity completed

---
+++ script.js
@@ -0,0 +1,7 @@
+let historyValue = document.getElementById("history-value");
+
+let outputValue = document.getElementById("output-value");
+
+let clearButton = document.getElementById("clear");
+let baskspaceButton = document.getElementById("backspace");
+let equalsButton = document.getElementById("=");
